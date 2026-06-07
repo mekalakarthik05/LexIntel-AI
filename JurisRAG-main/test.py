3ea22f64cd046b6c1b23590a3001c4c0179979bc
Karthik Mekala <mekalakarthik05@gmail.com>
2026-06-07 19:28:46 +0530

    Delete JurisRAG-main directory

--- JurisRAG-main/test.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('all-mpnet-base-v2')
-sentences = ["What is the capital of France?", "Paris is the capital of France.", "The weather is nice today."]
-embeddings = model.encode(sentences)
-print(util.cos_sim(embeddings[0], embeddings[1]))  # Should be high
-print(util.cos_sim(embeddings[0], embeddings[2]))  # Should be low
